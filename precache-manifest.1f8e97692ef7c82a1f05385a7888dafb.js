77856c728e8942f306d5308e5700be94dbcabb99
Sebastiaan Jansen <contact@sebastiaanjansen.be>
2020-04-06 16:54:32 +0200

    Updates

---
+++ precache-manifest.1f8e97692ef7c82a1f05385a7888dafb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d8f1bcbdbc76fa5ef77670f55886180",
+    "url": "/oikos/index.html"
+  },
+  {
+    "revision": "bf74b5061034306d8501",
+    "url": "/oikos/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1826aad140806e60e6d2",
+    "url": "/oikos/static/js/2.6169cb9e.chunk.js"
+  },
+  {
+    "revision": "f9416c2fa5dace694260608407c8cf2e",
+    "url": "/oikos/static/js/2.6169cb9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf74b5061034306d8501",
+    "url": "/oikos/static/js/main.4532a462.chunk.js"
+  },
+  {
+    "revision": "fca9d064411392530437",
+    "url": "/oikos/static/js/runtime-main.4c755ecb.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/oikos/precache-manifest.1f8e97692ef7c82a1f05385a7888dafb.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/oikos/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
